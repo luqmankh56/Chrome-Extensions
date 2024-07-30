b1288d13ddd4a34e7bd64c5e7e70d7058f88f694
Luqman Khan <158295061+luqmankh56@users.noreply.github.com>
2024-07-31 03:35:26 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
-
 let myLeads = []
 const inputEl = document.getElementById("input-el")
-const inputBtn= document.getElementById("input-btn")
+const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
 
-let name= localStorage.getItem("myLeads")
-console.log(myLeads)
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
-inputBtn.addEventListener("click", function(){
-    myLeads.push(inputEl.value)
+if (leadsFromLocalStorage) {
+myLeads = leadsFromLocalStorage
+renderLeads()
+}
+
+inputBtn.addEventListener("click", function() {
+    let lead = inputEl.value
+    if (!lead.startsWith("http://") && !lead.startsWith("https://")) {
+    lead = "http://" + lead
+    }
+    myLeads.push(lead)
     inputEl.value = ""
-    console.log(myLeads)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    renderLeads()
+})
+deleteBtn.addEventListener("click", function() {
+    myLeads = []
+    localStorage.removeItem("myLeads")
     renderLeads()
 })
 
 function renderLeads() {
     let listItems = ""
-    for(let i=0; i < myLeads.length; i++) {
-       listItems += `
-            <li>
+    for (let i = 0; i < myLeads.length; i++) {
+    listItems += `
+        <li>
             <a target='_blank' href='${myLeads[i]}'>
-                ${myLeads[i]}
-                </a>
-                </li>`
+            ${myLeads[i]}
+            </a>
+        </li>`
     }
-    ulEl.innerHTML= listItems
+    ulEl.innerHTML = listItems
 }
-renderLeads
+
+renderLeads()
