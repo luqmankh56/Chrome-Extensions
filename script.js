10dbefeb1ad8be822729eb29e100ab56bf8e30c6
Luqman Khan <158295061+luqmankh56@users.noreply.github.com>
2024-07-31 02:13:05 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
-
 let myLeads = []
 const inputEl = document.getElementById("input-el")
-const inputBtn= document.getElementById("input-btn")
+const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
-inputBtn.addEventListener("click", function(){
-    addLead()
+inputBtn.addEventListener("click", function() {
+addLead()
 })
 
 inputEl.addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-        addLead()}
+    if (event.key === "Enter"){
+    addLead()
+    }
 })
-   function addLead() {
+function addLead(){
     myLeads.push(inputEl.value)
     inputEl.value = ""
-    console.log(myLeads)
     renderLeads()
-   }
-
+}
 function renderLeads() {
     let listItems = ""
-    for(let i=0; i < myLeads.length; i++) {
-       listItems += `
-            <li>
-            <a target='_blank' href='${myLeads[i]}'>
-                ${myLeads[i]}
-                </a>
-                </li>`
+    for (let i = 0; i < myLeads.length; i++) {
+    listItems += `
+        <li>
+        <a target='_blank' href='${myLeads[i]}'>
+            ${myLeads[i]}
+        </a>
+        </li>`
     }
-    ulEl.innerHTML= listItems
+    ulEl.innerHTML = listItems
 }
-renderLeads
